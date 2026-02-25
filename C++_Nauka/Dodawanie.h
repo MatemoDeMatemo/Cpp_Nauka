213b0823c8662a42fa383b28fd777ec63921effc
Mateusz Draga <mateusz.draga.08@gmail.com>
2026-02-25 18:16:57 +0100

    Poprawka

--- C++_Nauka/Dodawanie.h
+++ C++_Nauka/Dodawanie.h
@@ -1,5 +1,5 @@
-#ifndef Dodawanie_H
-#define Dodawanie_H
+#ifndef DODAWANIE_H  // jesli funkcja ta nie jest zdefiniowana
+#define DODAWANIE_H  // zdefiniuj ta funkcje
 
 int Dodawanie(int x, int y);
 
