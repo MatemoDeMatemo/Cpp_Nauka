5cf50ffe24d9f94b12ffdadcc61bee3b3bd536ff
MatemoDeMatemo <121318467+MatemoDeMatemo@users.noreply.github.com>
2026-03-06 17:37:38 +0100

    Update 1_Hello_World.cpp

--- C++_Wprowadzenie/1_Hello_World.cpp
+++ C++_Wprowadzenie/1_Hello_World.cpp
@@ -1,10 +1,16 @@
-// 0] Hello World
-#include <iostream>
+#include <iostream>   // Makro - definiowane na poczatku kodu. Mozna deklarowac tutaj paczki kodu, funkcje, wartosci oraz wiecej.
+
+// Sercem kodu w C++ jest zawartosc funkcji main(). To co sie w niej znajdzie zostanie wykonane. 
 int main() {
-   std::cout << "Hello, World!" << std::endl;
-   return 0;
+   // cout to odpowiednik ~print(). znajduje sie w przestrzeni - "paczce" std. 
+   // << – operator przesyłania danych do strumienia
+   std::cout << "Hello, World!" << std::endl; 
+   
+   return 0; // Kod konczymy return 0. Jesli funkcja zwroci nam 0, bedziemy wiedzieli ze wykonala sie poprawnie w calosci.
 }
-// Komentarze
-/* Komentarz
+// By tworzyc komentarze stosujemy: // lub dla segmentow:
+/* 
 */
+
+// Najlatwiej jest zaznaczyc fragment kodu i uzyc skrotu "ctrl + /"
 
